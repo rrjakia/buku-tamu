38f39318ab437bda807f16a15c9defff6f2cd534
rrjakia <hasnacjr321@gmail.com>
2026-09-21 10:47:07 +0700

    templating app buku tamu dengan sb admin 2

---
+++ index.php
@@ -0,0 +1,23 @@
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                </div>
+                <!-- /.container-fluid -->
+
+                <?php
+                include_once('templates/header.php');
+                ?>
+
+                <!-- begin page content -->
+                 <div class="container-fluid">
+
+                    <!-- page heading -->
+                     <h1 class="h3 mb-4 text-gray-800">Dashboard Admin</h1>
+
+                 </div>
+                 <!-- /.container-fluid -->
+
+                 <?php 
+                 include_once('templates/footer.php')
+                 ?>
+
