074f496403fba2ee2dadba1098aff60d99954e1a
rrjakia <hasnacjr321@gmail.com>
2026-09-24 15:41:01 +0700

    menambahkan modal dan fitur tambah data tamu

--- function.php
+++ function.php
@@ -1,7 +1,9 @@
 <?php
+
 require_once('koneksi.php');
 
-function query($query) {
+function query($query)
+{
     global $koneksi;
 
     $result = mysqli_query($koneksi, $query);
@@ -17,4 +19,40 @@ function query($query) {
     }
 
     return $rows;
+}
+
+
+function tambah_tamu($data)
+{
+    global $koneksi;
+
+    $query = mysqli_query(
+        $koneksi,
+        "SELECT MAX(id_tamu) AS kodeTerbesar FROM buku_tamu"
+    );
+
+    $dataTerbesar = mysqli_fetch_assoc($query);
+
+    if ($dataTerbesar['kodeTerbesar'] == null) {
+        $kode = 1;
+    } else {
+        $kode = $dataTerbesar['kodeTerbesar'] + 1;
+    }
+
+    $tanggal = date("Y-m-d");
+
+    $nama_tamu = htmlspecialchars($data["nama_tamu"]);
+    $alamat = htmlspecialchars($data["alamat"]);
+    $no_hp = htmlspecialchars($data["no_hp"]);
+    $bertemu = htmlspecialchars($data["bertemu"]);
+    $kepentingan = htmlspecialchars($data["kepentingan"]);
+
+    $query = "INSERT INTO buku_tamu
+              (id_tamu, tanggal, nama_tamu, alamat, no_hp, bertemu, kepentingan)
+              VALUES
+              ('$kode', '$tanggal', '$nama_tamu', '$alamat', '$no_hp', '$bertemu', '$kepentingan')";
+
+    mysqli_query($koneksi, $query);
+
+    return mysqli_affected_rows($koneksi);
 }
--- koneksi.php
+++ koneksi.php
@@ -5,6 +5,9 @@ define('USER_NAME', 'root');
 define('PASSWORD', '');
 define('DB_NAME', 'app_bukutamu');
 
-$koneksi = mysqli_connect(HOST_NAME, USER_NAME, PASSWORD, DB_NAME);
-
-?>
+$koneksi = mysqli_connect(
+    HOST_NAME,
+    USER_NAME,
+    PASSWORD,
+    DB_NAME
+);
