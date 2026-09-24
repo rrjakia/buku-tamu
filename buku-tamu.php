59ab25e3589a08453a0678849837dd207b30436b
rrjakia <hasnacjr321@gmail.com>
2026-09-24 22:16:25 +0700

    menambahkan halaman hapus tamu dan fitur hapus data tamu

--- buku-tamu.php
+++ buku-tamu.php
@@ -114,17 +114,8 @@ include_once('templates/header.php');
                                 <td><?= $tamu['kepentingan']; ?></td>
 
                                 <td>
-
-                                    <a class="btn btn-success"
-                                        href="edit-tamu.php?id=<?= $tamu['id_tamu']; ?>">
-                                        Ubah
-                                    </a>
-
-                                    <button class="btn btn-danger"
-                                        type="button">
-                                        Hapus
-                                    </button>
-
+                                    <a class="btn btn-success" href="edit-tamu.php?id=<?= $tamu['id_tamu'] ?>">Ubah</a>
+                                    <a onclick="return confirm('Apakah anda yakin ingin menghapus data ini?')" class="btn btn-danger" href="hapus-tamu.php?id=<?= $tamu['id_tamu'] ?>">Hapus</a>
                                 </td>
 
                             </tr>
--- function.php
+++ function.php
@@ -81,3 +81,17 @@ function ubah($data)
 
     return mysqli_affected_rows($koneksi);
 }
+
+// function hapus data tamu
+function hapus_tamu($id)
+{
+    global $koneksi;
+
+    $query = "DELETE FROM buku_tamu WHERE id_tamu = '$id'";
+
+    mysqli_query($koneksi, $query);
+
+    return mysqli_affected_rows($koneksi);
+}
+
+echo "FUNCTION TERBACA";
